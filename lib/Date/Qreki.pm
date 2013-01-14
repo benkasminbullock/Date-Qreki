eb7662e6a17c094177b02e8eed7145f52b810498
Ben Bullock <benkasminbullock@gmail.com>
2013-01-14 14:19:02 +0900

    Initial commit of Date-Qreki

---
+++ lib/Date/Qreki.pm
@@ -0,0 +1,13 @@
+=head1 NAME
+
+Date::Qreki
+
+=cut
+package Date::Qreki;
+require Exporter;
+@ISA = qw(Exporter);
+@EXPORT_OK = qw//;
+use warnings;
+use strict;
+our $VERSION = 0.01;
+1;
---
+++ t/Date-Qreki.t
@@ -0,0 +1,9 @@
+use warnings;
+use strict;
+use Test::More tests => 1;
+BEGIN { use_ok('Date::Qreki') };
+use Date::Qreki;
+
+# Local variables:
+# mode: perl
+# End:
