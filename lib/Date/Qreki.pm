692dd0f10ce9fff78b476433473fae8178b8f4a2
Ben Bullock <benkasminbullock@gmail.com>
2014-02-12 09:38:34 +0900

    Add originals to repo, add licence translation.

--- lib/Date/Qreki.pm
+++ lib/Date/Qreki.pm
@@ -1,8 +1,17 @@
+=encoding UTF-8
+
 =head1 NAME
 
 Date::Qreki - convert Gregorian to Japanese "kyureki" dates.
 
 =head1 SYNOPSIS
+
+This module is currently undocumented, so to use it you need to read
+the source code. If you are interested in helping write documentation
+for the module or would like to discuss, please contact the module
+author or visit
+L<Questhub|http://questhub.io/realm/perl/quest/5249010053a57fbb3b00002e>
+and vote for this project.
 
 =head1 DESCRIPTION
 
@@ -14,9 +23,84 @@ Date::Qreki - convert Gregorian to Japanese "kyureki" dates.
 
 =head1 SEE ALSO
 
-=head1 COPYRIGHT AND LICENSE
+=head1 COPYRIGHT
+
+Date::Qreki is copyright H. Takano, N. Ueno.
+
+=head1 LICENSE
+
+The original licence in Japanese reads as follows:
+
+19.  配布規定について
+
+    本当は、「一般常識の許す範囲で...」としたい所ですが、世の中には色々な人
+  がいますので、ある程度の枠組みが必要なのは仕方の無いことなのでしょう。 と
+  もあれ拙者が希望するのは以下のようなものです。
+
+     i   本スクリプト・説明書を再配布する場合はオリジナルのアーカイブファイ
+       ルに含まれるファイルを全て含み、オリジナルのスクリプト・説明書を改変
+       しないで下さい。 また、 頒布の都合でアーカイブ形式（.arc  .zip  .zoo
+       等）を変更する場合も同様に扱って下さい。
+
+    ii   再配布する際の媒体に要するコストを除いて一切の金銭等の授受は禁止い
+       たします。    （他の言語に移植したものを配布する場合にも適用いたしま
+       す。）
+
+   iii   他の言語に移植したり、  本スクリプトの一部または全部を引用して作成
+       し、これを配布する場合には、オリジナルのスクリプトと本説明書を必ず同
+       梱して下さい。
+       その場合の著作権につきましては、引用した部分のみ著者に帰属し、その外
+       は製作者に帰属します。
+
+    iv   配布によって著作者が一切の制限を受ける可能性がないようにして下さ
+       い。
+
+  なお、 いづれの場合におきましても、承諾・転載の報告など一切不要といたしま
+  す。
+
+Translation:
+
+19 Conditions for redistribution
+
+I would prefer to release this under the conditions of "whatever
+normal common sense allows", but since there are a variety of people
+in the world, unfortunately I think some form of framework is
+necessary. My wishes are as follows
+
+i. If this script is redistributed, please also include all the
+original scripts, and do not modify the original script and its
+documentation. This also applies if the archive format (.arc, .zip,
+.zoo, etc.) is changed.
+
+ii. I forbid any resale of this program beyond the cost of
+distribution media. (This also applies if the program is translated to
+a different programming language).
+
+iii. If this is translated to a different programming language, or if
+part or all of this script is used as part of another program, please
+be sure to include the original script and this explanation with
+it. If part of this program is used, I retain the copyright of the
+quoted material only and the remaining program remains copyrighted by
+its author.
+
+iv. Please do not allow the author of this software to be placed under
+any restrictions by your redistribution.
+
+In any case, there is absolutely no need to obtain the author's
+consent or notify the author.
+
+End of artistic translation.
+
+The original Awk script and its documentation are in F<originals> in
+the distribution.
 
 =head1 AUTHOR
+
+Original Awk script by H. Takano. Perl conversion by N. Ueno. This
+CPAN-ification of N. Ueno's Perl script was performed by Ben Bullock.
+
+For enquiries about this Perl module, please contact Ben Bullock
+<bkb@cpan.org>.
 
 =cut
 
@@ -26,7 +110,7 @@ require Exporter;
 @EXPORT_OK = qw/calc_kyureki get_rokuyou/;
 use warnings;
 use strict;
-our $VERSION = 0.01;
+our $VERSION = 0.05;
 
 #=========================================================================
 # 旧暦計算サンプルプログラム  $Revision:   1.1  $
